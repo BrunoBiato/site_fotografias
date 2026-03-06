f6d5647c45a58a34fd8313fe2e98bc78dcfdc3ee
Biato <brunobiatolp@gmail.com>
2026-03-05 22:19:45 -0300

    Primeiro commit do site

---
+++ js/script.js
@@ -0,0 +1,37 @@
+// Lógica da Janela
+const btnClose = document.getElementById('btn-fechar');
+const janelaPrincipal = document.getElementById('janela-principal');
+const msgFechado = document.getElementById('msg-fechado');
+const btnRecarregar = document.getElementById('btn-recarregar');
+
+if (btnClose && janelaPrincipal) {
+    btnClose.addEventListener('click', () => {
+        janelaPrincipal.style.display = 'none';
+        msgFechado.style.display = 'block';
+    });
+}
+
+if (btnRecarregar) {
+    btnRecarregar.addEventListener('click', () => {
+        janelaPrincipal.style.display = 'flex';
+        msgFechado.style.display = 'none';
+    });
+}
+
+// Lógica do Player de Música
+const audio = document.getElementById('bg-audio');
+const btnPlay = document.getElementById('btn-play');
+const btnPause = document.getElementById('btn-pause');
+
+if(btnPlay && audio) {
+    btnPlay.addEventListener('click', () => {
+        // Tenta tocar a música. Se o ficheiro não for encontrado, mostra um erro.
+        audio.play().catch(e => alert("Coloque o ficheiro de áudio correto na pasta para tocar!"));
+    });
+}
+
+if(btnPause && audio) {
+    btnPause.addEventListener('click', () => {
+        audio.pause();
+    });
+}
